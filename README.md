2c85e8f8bd3f8dff5e5e9b02416d211c1dbdb223
Ryan Miller <rymiller@icct.com>
2017-01-23 09:02:37 -0500

    Initial checkin

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# TextSpeaker
+# Text Speaker
+Super simple text reader console application that reads in text from script.txt line by line and speaks it using System.Speech.Synthesis. System.Speech.Synthesis has been in the .NET Framework since 3.0.
+
---
+++ TextSpeaker/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Speech.Synthesis;
+using System.IO;
+
+namespace TextSpeaker
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				using (SpeechSynthesizer synth = new SpeechSynthesizer())
+				{
+					synth.SetOutputToDefaultAudioDevice();
+
+					using (StreamReader sr = new StreamReader("script.txt"))
+					{
+						while (!sr.EndOfStream)
+						{
+							String line = sr.ReadLine();
+							Console.WriteLine(line);
+
+							synth.Speak(line);
+
+							Console.ReadKey();
+						}
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("The file could not be read:");
+				Console.WriteLine(e.Message);
+			}
+			Console.WriteLine();
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
+
+		}
+	}
+}
